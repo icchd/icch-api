2dfeda675436916b3346d62440045e631ae8f72e
Savio Dimatteo <darksmo@gmail.com>
2018-06-09 07:46:04 +0200

    update app port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
 
-var sPortEnvVarName = process.env.PORT_ENVVAR_NAME;
+var sPortEnvVarName = process.env.app_port || process.env.PORT_ENVVAR_NAME;
 
 var server = app.listen(process.env[sPortEnvVarName] || 9999, function () {
     console.log("Listening on port %s...", server.address().port);
