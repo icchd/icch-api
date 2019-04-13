20f67acee69d2c916fab676d42d0d920e919ed40
Savio Dimatteo <savio.dimatteo@sap.com>
2019-04-13 14:10:10 +0200

    Add link with the readings and better e-mail formatting

--- scheduleChecker.js
+++ scheduleChecker.js
@@ -247,6 +247,8 @@ function prepareEmailBodyHTML (oNextSundayRecord) {
         }
     }
 
+    const readingLink = `http://www.usccb.org/bible/readings/${oNextSundayRecord.date.format("MMDDYY")}.cfm`;
+
     return `<br />
 <b>Priest</b> &rarr; ${oNextSundayRecord.priest.join(", ")}<br />
 <br />
@@ -256,6 +258,7 @@ function prepareEmailBodyHTML (oNextSundayRecord) {
 <br />
 <b>Lectors:</b><br />
 ${formatLectors(oNextSundayRecord.lector)}
+You can find the readings <a href="${readingLink}">here</a>.<br />
 <br />
 <b>Eucharistic Minister</b> &rarr; ${oNextSundayRecord.em.join(", ")}<br />
 `;
